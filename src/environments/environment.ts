40722cf6ae8a5a7756076a0dfdb5c2e9d8e7e6ac
llighter <hadbsgk@gmail.com>
2020-06-24 20:39:31 +0900

    Remove: hide environments for security

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false
+  , firebaseConfig: {
+    apiKey: "AIzaSyB9-Gp5ii5jTrXBlyMB94UN-q-0YeLsIRw",
+    authDomain: "studycard-251e3.firebaseapp.com",
+    databaseURL: "https://studycard-251e3.firebaseio.com",
+    projectId: "studycard-251e3",
+    storageBucket: "studycard-251e3.appspot.com",
+    messagingSenderId: "549242650630",
+    appId: "1:549242650630:web:82220a0a2d207d22368387"
+  }
 };
 
 /*
