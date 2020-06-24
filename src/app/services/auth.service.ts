e860d37cf8a1fa82ed6adf9b907230f7bf188f67
llighter <hadbsgk@gmail.com>
2020-06-24 21:42:39 +0900

    Implement: Auth, Db service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,136 @@
+import { auth } from 'firebase/app';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { DbService } from './db.service';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Platform, LoadingController } from '@ionic/angular';
+
+import { Observable, of } from 'rxjs';
+import { switchMap, take, map } from 'rxjs/operators';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$: Observable<any>;
+
+  constructor(
+    private afauth: AngularFireAuth
+    , private db: DbService
+    , private router: Router
+    , private storage: Storage
+    , private gplus: GooglePlus
+    , private platform: Platform
+    , private loadingController: LoadingController
+  ) {
+    this.user$ = this.afauth.authState.pipe(
+      switchMap(user => (user ? db.doc$(`users/${user.uid}`) : of(null)))
+    );
+
+    this.handleRedirect();
+  }
+
+  uid() {
+    return this.user$
+      .pipe(
+        take(1),
+        map(u => u && u.uid)
+      )
+      .toPromise();
+  }
+
+  async anonymousLogin() {
+    const credential = await this.afauth.signInAnonymously();
+    return await this.updateUserData(credential.user);
+    
+  }
+
+  private updateUserData({ uid, email, displayName, photoURL, isAnonymous }) {
+    const path = `users/${uid}`;
+
+    const data = {
+      uid
+      , email
+      , displayName
+      , photoURL
+      , isAnonymous
+    };
+
+    this.db.updateAt(path, data);
+    return this.router.navigate(['/']);
+  }
+
+  async signOut() {
+    await this.afauth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  // GOOGLE AUTH
+
+  setRedirect(val) {
+    this.storage.set('authRedirect', val);
+  }
+
+  async isRedirect() {
+    return await this.storage.get('authRedirect');
+  }
+
+  async googleLogin() {
+    try {
+      let user;
+
+      if (this.platform.is('cordova')) {
+        user = await this.nativeGoogleLogin();
+        console.log('googleLogin()');
+      } else {
+        await this.setRedirect(true);
+        const provider = new auth.GoogleAuthProvider();
+        user = await this.afauth.signInWithRedirect(provider);
+      }
+
+      console.table(user);
+      return await this.updateUserData(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Handle login with redirect for web google auth
+  private async handleRedirect() {
+    if ((await this.isRedirect()) !== true) {
+      return null;
+    }
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const result = await this.afauth.getRedirectResult();
+
+    if (result.user) {
+      await this.updateUserData(result.user);
+    }
+    await loading.dismiss();
+
+    await this.setRedirect(false);
+
+    return result;
+  }
+
+  async nativeGoogleLogin(): Promise<any> {
+    console.log('nativeGOogleLogin()');
+    // const gplusUser = await this.gplus.login({
+    //   webClientId:
+    //     '549242650630-r48mvl89ddfd2ir6vvrlgaabahmbpb6m.apps.googleusercontent.com',
+    //   offline: true,
+    //   scope: 'profile email'
+    // });
+    // TODO: Check the option if necessary
+    const gplusUser = await this.gplus.login({});
+
+    console.table(gplusUser);
+    return await this.afauth.signInWithCredential(
+      auth.GoogleAuthProvider.credential(gplusUser.idToken)
+    );
+  }
+}
---
+++ src/app/services/db.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  collection$ (path, query?) {
+    return this.afs
+      .collection(path, query)
+      .snapshotChanges()
+      .pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data: Object = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+        })
+      );
+  }
+
+  doc$ (path) {
+    return this.afs
+      .doc(path)
+      .snapshotChanges()
+      .pipe(
+        map(doc => {
+          // TODO: Check doc.payload.data
+          return { id: doc.payload.id, ...doc.payload.data };
+        })
+      );
+  }
+
+  /**
+   * @param {string} path 'collection' or 'collection/docID'
+   * @param {object} data new data
+   *
+   * Create or updates data on a collection or document.
+   */
+  updateAt(path: string, data: Object): Promise<any> {
+    const segments = path.split('/').filter(v => v);
+    if (segments.length % 2) {
+      // Odd is always a collection
+      return this.afs.collection(path).add(data);
+    } else {
+      return this.afs.doc(path).set(data, { merge: true });
+    }
+  }
+
+  /**
+   * @param {string} path path to document
+   *
+   * Deletes document from Firestore
+   */
+  delete(path: string) {
+    return this.afs.doc(path).delete();
+  }
+}
+
+
