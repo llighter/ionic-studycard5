25639df7ae2844f6cbb2910a2561ed3dcbb0871e
llighter <hadbsgk@gmail.com>
2020-08-23 23:02:36 +0900

    Update: Add badge to fab to show count of stage 0(#33)

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -10,11 +10,16 @@ import { DeckFormComponent } from './deck-form/deck-form.component';
 import { DeckDetailComponent } from './deck-detail/deck-detail.component';
 import { CardFormComponent } from './card-form/card-form.component';
 
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule } from "@angular/material/badge";
+
 @NgModule({
   imports: [
+    FormsModule,
     IonicModule,
     CommonModule,
-    FormsModule,
+    MatIconModule,
+    MatBadgeModule,
     ReactiveFormsModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
